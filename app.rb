c8e89844ce67252c2df7722126c1f4809f5a8f44
Gor <7495500@gmail.com>
2020-04-19 20:28:52 +0300

    Init barbers table db

--- app.rb
+++ app.rb
@@ -1,25 +1,47 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-# require 'sqlite3'
+require 'sqlite3'
 
-# def get_db
-# 	db = SQLite3::Database.new 'sinatra_barber_shop.sqlite'
-# 	db.results_as_hash = true
-# 	return db
-# end
-#
-# configure do
-# 	db = get_db
-# 	db.execute CREATE TABLE IF NOT EXISTS "Users" (
-# 		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-# 	 	"username" TEXT,
-# 	 	"phone" TEXT,
-# 	 	"datestamp" TEXT,
-# 	 	"barber" TEXT,
-# 	 	"color" TEXT
-# 	);
-# end
+def is_barber_exists? db, name
+	db.execute('SELECT * FROM Barbers where name = ?', [name]).length > 0
+end
+
+def seed_db db, barbers
+
+	barbers.each do |barber|
+		if !is_barber_exists? db, barber
+			db.execute 'INSERT INTO Barbers (name)
+			VALUES (?)', [barber]
+		end
+	end
+
+end
+
+def get_db
+	db = SQLite3::Database.new 'sinatra_barber_shop.sqlite'
+	db.results_as_hash = true
+	return db
+end
+
+configure do
+	db = get_db
+	db.execute CREATE TABLE IF NOT EXISTS "Users" (
+		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	 	"username" TEXT,
+	 	"phone" TEXT,
+	 	"datestamp" TEXT,
+	 	"barber" TEXT,
+	 	"color" TEXT
+	);
+
+	db.execute CREATE TABLE IF NOT EXISTS "Barbers" (
+		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+		"name" TEXT
+	);
+
+	seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'John Smith']
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
@@ -63,18 +85,18 @@ post '/visit' do
 
 	end
 
-	# db = get_db
-	# db.execute 'INSERT INTO Users (username, phone, datestamp, barber, color)
-	# VALUES (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
+	db = get_db
+	db.execute 'INSERT INTO Users (username, phone, datestamp, barber, color)
+	VALUES (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
 
 	erb :visit
 end
 
 get '/showusers' do
-	# db = git_db
-	#
-	#@results = db.execute 'SELECT * FROM Users
-	# ORDER BY id DESC'
+	db = git_db
+
+	@results = db.execute 'SELECT * FROM Users
+	ORDER BY id DESC'
 
 	erb :showusers
 end
